93eb20f4470176d72f6a0f2ae5fca3959a54a2a2
Kieren Beckett <mail@kierenb.net>
2018-10-25 09:06:21 +0100

    Support running after version of survey runner changes

--- main.py
+++ main.py
@@ -22,9 +22,13 @@ SLACK_WEBHOOK = os.getenv('SLACK_WEBHOOK')
 SLACK_CHANNEL = os.getenv('SLACK_CHANNEL', '#catd')
 
 NUM_WORKERS = int(os.getenv('NUM_WORKERS', '1'))
-SUBMISSIONS = os.getenv('SUBMISSIONS', None)
-if SUBMISSIONS:
-    SUBMISSIONS = int(SUBMISSIONS)
+
+MODE_CONTINUOUS = 'continuous'
+MODE_AFTER_DEPLOY = 'after_deploy'
+MODE_ONE_OFF = 'one_off'
+MODE = os.getenv('MODE', MODE_CONTINUOUS)
+
+SUBMISSIONS = int(os.getenv('SUBMISSIONS', '1'))
 
 WAIT_BETWEEN_PAGES = int(os.getenv('WAIT_BETWEEN_PAGES', '5'))
 PAGE_LOAD_TIME_SUCCESS = float(os.getenv('PAGE_LOAD_TIME_SUCCESS', '1.2'))
@@ -34,8 +38,8 @@ log = logging.getLogger(__name__)
 
 def worker(worker_id):
     page_load_times = []
-    num_submissions = SUBMISSIONS
-    while num_submissions is None or num_submissions > 0:
+    num_submissions = SUBMISSIONS if MODE != MODE_CONTINUOUS else 1
+    while num_submissions > 0:
         try:
             start_time = time.time()
             log.info('[%d] Starting survey', worker_id)
@@ -44,13 +48,21 @@ def worker(worker_id):
             page_load_times += session.page_load_times
             average_page_load_time = sum(session.page_load_times) / len(session.page_load_times)
             log.info('[%d] Survey completed in %f seconds, average page load time was %.2f seconds', worker_id, time.time() - start_time, average_page_load_time)
-            if num_submissions is not None:
+            if MODE != MODE_CONTINUOUS:
                 num_submissions -= 1
         except Exception:
             log.exception('Error running session, will retry in 30 seconds')
             time.sleep(30)
 
     return page_load_times
+
+
+def get_version():
+    try:
+        return requests.get(SURVEY_RUNNER_URL + '/status').json()['version']
+    except Exception:
+        log.exception('Error getting version')
+        return None
 
 
 def announce_results(message, color):
@@ -78,12 +90,11 @@ def announce_results(message, color):
     log.info('Called slack webhook, response code %d', resp.status_code)
 
 
-if __name__ == '__main__':
-
+def run_workers():
     log.info(
         'Running %d workers each making %s submissions waiting %d seconds between pages',
         NUM_WORKERS,
-        str(SUBMISSIONS) if SUBMISSIONS else 'unlimited',
+        str(SUBMISSIONS) if MODE != MODE_CONTINUOUS else 'unlimited',
         WAIT_BETWEEN_PAGES
     )
 
@@ -101,8 +112,28 @@ if __name__ == '__main__':
         'The average page load time was *{:.2f}* seconds\n_{} workers each making {} submissions waiting {} seconds between pages_'.format(
             average_page_load_time,
             NUM_WORKERS,
-            SUBMISSIONS if SUBMISSIONS else 'unlimited',
+            SUBMISSIONS,
             WAIT_BETWEEN_PAGES
         ),
         "#D00000" if average_page_load_time > PAGE_LOAD_TIME_SUCCESS else "00D000"
     )
+
+
+if __name__ == '__main__':
+
+    run_workers()
+
+    if MODE == MODE_AFTER_DEPLOY:
+        tested_version = current_version = get_version()
+
+        while True:
+            while current_version is None or current_version == tested_version:
+                log.info('Version has not changed from %s, waiting', tested_version)
+                time.sleep(30)
+                current_version = get_version()
+
+            log.info('Version has changed from %s to %s, repeating tests', tested_version, current_version)
+
+            run_workers()
+
+            tested_version = current_version
