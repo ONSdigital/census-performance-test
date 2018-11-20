023563456799abc0e348da1c9b57f5cf315c1256
Kieren Beckett <mail@kierenb.net>
2018-11-20 14:58:32 +0000

    remove newrelic

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import logging;logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
-import newrelic.agent; newrelic.agent.initialize()
 
 import time
 
